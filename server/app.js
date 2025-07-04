c4440e0d7c1dd0a23b3caa7b297188da34608ef8
Rehan2007ahmad <rehan1000ahmad@gmial.com>
2025-07-04 21:39:33 +0545

    vercel

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use('/api/category', require('./routes/category.routes'))
 app.use('/api/role', require('./routes/role.routes'))
 app.use('/api/post', require('./routes/post.routes'))
 
-app.listen(process.env.PORT ,()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
-})
+// app.listen(process.env.PORT ,()=>{
+//     console.log(`server is running on port ${process.env.PORT}`)
+// })
+module.exports = app;
